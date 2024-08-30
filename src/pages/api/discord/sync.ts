5910af9f67743612b91bb13abda7355ca11919f2
MustqfaCan <mustafacangoktas51@gmail.com>
2024-08-30 13:46:30 +0300

    Add login redirect to discord sync

--- src/pages/api/discord/sync.ts
+++ src/pages/api/discord/sync.ts
@@ -1,3 +1,5 @@
+import AuthManager from "@/lib/server/auth/AuthManager";
+import JWTManager from "@/lib/server/auth/JWTManager";
 import DiscordOauth2Manager from "@/lib/server/discord/DiscordOauth2Manager";
 import { NextApiRequest, NextApiResponse } from "next/types";
 
@@ -8,6 +10,18 @@ export const config = {
 };
 
 export default async function Login(req: NextApiRequest, res: NextApiResponse) {
+    const sessionToken = JWTManager.getInstance().getSessionTokenFromJWT(req.cookies["orleans.token"] || "");
+
+    if (!sessionToken) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const user = await AuthManager.getInstance().getUserFromSessionToken(sessionToken);
+
+    if (!user) {
+        return res.redirect("/giris-yap");
+    }
+
     const url = DiscordOauth2Manager.getInstance().generateAuthUrl();
     res.redirect(url);
 };
